512dc31323e8e88dedc91f49c1af4f7b81db5f88
Thomas Revell <thomasprevell@gmail.com>
2023-09-28 14:07:26 +0100

    Finish database setup script

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -1,34 +1,92 @@
 -- Create all tables
-CREATE TABLE isa_types
+CREATE TABLE isaType
 (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL DEFAULT NULL,
     name VARCHAR(255) NOT NULL,
-    UNIQUE INDEX(name)
+    UNIQUE INDEX(name),
+    PRIMARY KEY(id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE deposit_limits
+CREATE TABLE depositLimit
 (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL DEFAULT NULL,
-    isa_type_id INT UNSIGNED NOT NULL,
     tax_year_end SMALLINT UNSIGNED NOT NULL,
     annual_limit DECIMAL(8,2) UNSIGNED NOT NULL,
-    UNIQUE INDEX(isa_type_id, tax_year_end)
+    UNIQUE INDEX(tax_year_end),
+    PRIMARY KEY(id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE products
+CREATE TABLE product
 (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL DEFAULT NULL,
-    name VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
     isa_type_id INT UNSIGNED NOT NULL,
-    flexible BOOLEAN NOT NULL
+    UNIQUE INDEX(name),
+    PRIMARY KEY(id),
+    FOREIGN KEY(isa_type_id) REFERENCES isaType(id) ON UPDATE CASCADE ON DELETE RESTRICT
+) ENGINE=InnoDB;
+
+CREATE TABLE fund
+(
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL DEFAULT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    UNIQUE INDEX(name),
+    PRIMARY KEY(id)
+) ENGINE=InnoDB;
+
+CREATE TABLE customer
+(
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL DEFAULT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    UNIQUE INDEX(username),
+    PRIMARY KEY(id)
+) ENGINE=InnoDB;
+
+CREATE TABLE account
+(
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL DEFAULT NULL,
+    customer_id INT UNSIGNED NOT NULL,
+    product_id INT UNSIGNED NOT NULL,
+    active BOOLEAN NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(customer_id) REFERENCES customer(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(product_id) REFERENCES product(id) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE investment
+(
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL DEFAULT NULL,
+    account_id INT UNSIGNED NOT NULL,
+    fund_id INT UNSIGNED NOT NULL,
+    amount DECIMAL(8,2) UNSIGNED NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    FOREIGN KEY(account_id) REFERENCES account(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(fund_id) REFERENCES fund(id) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=InnoDB;
 
 -- Populate default application data
-INSERT INTO isa_types(id, name)
+INSERT INTO isaType(id, name)
 VALUES(1, 'Stocks and shares'), (2, 'Cash'), (3, 'Lifetime'), (4, 'Innovative Finance'), (5, 'Junior');
 
-INSERT INTO deposit_limits(id, isa_type_id, tax_year_end, annual_limit)
-VALUES(1, 1, 2024, 20000), (2, 2, 2024, 20000), (3, 3, 2024, 20000), (4, 4, 2024, 20000), (5, 5, 2024, 9000);
+INSERT INTO depositLimit(id, tax_year_end, annual_limit)
+VALUES(1, 2024, 20000);
 
-INSERT INTO products(id, name, isa_type_id, flexible)
-VALUES(1, 'Cushon ISA', 1, 0), (2, 'Cushon Flexible ISA', 1, 1);
+INSERT INTO product(id, name, isa_type_id)
+VALUES(1, 'Cushon ISA', 1), (2, 'Cushon Lifetime ISA', 3);
+
+INSERT INTO fund(id, name, description)
+VALUES
+    (1, 'Cushon Equities Fund', 'An equities fund for the Cushon recruitment scenario'),
+    (2, 'Example fund', 'An example fund for the Cushon recruitment scenario');
+
+
+-- Customer passwords are hashed using PHP's password_hash() function.
+-- The password is 'CushonTest2023!'
+INSERT INTO customer(id, username, password)
+VALUES(1, 'thomas.revell', '$2y$10$hgrGMZizE72OCSSg9nPVZuBFcl.grn2/61AsPIKn6RUiLMVciEp82');
+
+INSERT INTO account(id, customer_id, product_id, active)
+VALUES(1, 1, 1, 1);
